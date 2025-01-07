297aafc9c36ade6aae77d4926014c361bf0b8286
Akhand <singhakhandpratap136@gmail.com>
2025-01-07 15:51:15 +0530

    readme and template.py updated

--- template.py
+++ template.py
@@ -2,8 +2,6 @@ import os
 from pathlib import Path
 import logging
 
-import logging
-
 # Configure logging
 logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
 
